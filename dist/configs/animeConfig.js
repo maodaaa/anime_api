cbb4731ee405afed0baad0e5979efd3b93d1f671
maorid <maorid@bti.id>
2025-10-01 11:36:50 +0700

    refactor: update url

--- dist/configs/animeConfig.js
+++ dist/configs/animeConfig.js
@@ -3,8 +3,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const animeConfig = {
     PORT: 3001,
     baseUrl: {
-        otakudesu: "https://otakudesu.cloud",
-        samehadaku: "https://samehadaku.mba",
+        otakudesu: "https://otakudesu.best",
+        samehadaku: "https://v1.samehadaku.how",
     },
     response: {
         href: true,
